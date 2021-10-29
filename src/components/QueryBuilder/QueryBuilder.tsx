42ab4db34b74636488846ebc166b9bf5bf2b9a54
Daniel Blankenship <danielblankenship@danielblankenship.local>
2021-10-29 11:07:43 -0500

    add basic response styling

--- src/components/QueryBuilder/QueryBuilder.tsx
+++ src/components/QueryBuilder/QueryBuilder.tsx
@@ -1,11 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import QueryEntry from "../QueryEntry/QueryEntry";
 import { v4 as uuidv4 } from "uuid";
+import { Api } from "../../Api";
+import { CompanyInterface } from "../../interfaces/CompanyInterface";
+import { HighlightInterface } from "../../interfaces/HighlightInterface";
 
+type QueryContextType = {};
+type FieldsType = {
+  predicate: string;
+  operator: string;
+  query: string;
+};
 export const QueryContext = createContext({});
 
 const QueryBuilder = () => {
-  const handleQueryUpdate = (context: any, queryId: string, fields: any) => {
+  const [store, setStore] = useState([]);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      const store = await Api.fetchStore();
+      setStore(store);
+    };
+
+    fetchCompanies();
+  }, []);
+
+  const handleQueryUpdate = (
+    context: QueryContextType,
+    queryId: string,
+    fields: FieldsType
+  ) => {
     const query = {
       [queryId]: `predicate=${fields.predicate}&operator=${fields.operator}&query=${fields.query}`,
     };
@@ -23,32 +47,54 @@ const QueryBuilder = () => {
   return (
     <QueryContext.Provider value={queryMap}>
       <div className="QueryBuilder">
-        <section className="SearchBar">
-          <section className="Queries">
-            {queryEntries.map((query, i) => (
-              <div key={`query-${i}`} className="Query">
-                <span>{i === 0 ? "WHERE" : "AND"}</span> {query}
-              </div>
-            ))}
+        <section className="TopNav">
+          <section className="SearchBar">
+            <section className="Queries">
+              {queryEntries.map((query, i) => (
+                <div key={`query-${i}`} className="Query">
+                  <span>{i === 0 ? "where" : "and"}</span> {query}
+                </div>
+              ))}
+            </section>
+            <section>
+              <button
+                className="AddFilterBtn"
+                onClick={() => {
+                  setQueryEntries([
+                    ...queryEntries,
+                    <QueryEntry
+                      queryId={uuidv4()}
+                      onQueryUpdate={handleQueryUpdate}
+                    />,
+                  ]);
+                }}
+              >
+                + Add New Filter
+              </button>
+            </section>
           </section>
-          <button
-            onClick={() => {
-              setQueryEntries([
-                ...queryEntries,
-                <QueryEntry
-                  queryId={uuidv4()}
-                  onQueryUpdate={handleQueryUpdate}
-                />,
-              ]);
-            }}
-          >
-            Add New Filter
-          </button>
         </section>
-        <button>Search</button>
-        <div className="Response">
-          <pre>{JSON.stringify(queryMap, null, 2)}</pre>
-        </div>
+
+        <section className="QueryString">
+          <div className="QueryStringResponse">
+            <pre>{JSON.stringify(queryMap, null, 2)}</pre>
+          </div>
+        </section>
+
+        <section className="QueryResponse">
+          {store.map((company: CompanyInterface) => (
+            <div className="QueryResponseEntry">
+              <div className="QueryResponseEntryContent">
+                <div className="CompanyName">{company.name}</div>
+                <div className="CompanyHighlights">
+                  {company.highlights.map((highlight: HighlightInterface) => (
+                    <div className="Highlight">{highlight.category}</div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
       </div>
     </QueryContext.Provider>
   );
